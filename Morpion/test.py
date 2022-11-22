b331146b7c234ddb13ff147359b10ca900f42a77
Enzo P <ephilippe@gaming.tech>
2022-11-22 18:35:42 +0100

    t

--- Morpion/test.py
+++ Morpion/test.py
@@ -7,9 +7,8 @@ class Morpion(Tk):
         Tk.__init__(self)
         self.title("Plateau ")
  
-        self.can = Canvas(self, width = 420, height = 420)
+        self.can = Canvas(self, width = 420, height = 600)
         self.can.pack()
-        self.tracer_plateau()
         self.croix_image = PhotoImage(file='Morpion\img\croix.png')
         self.cercle_image = PhotoImage(file='Morpion\img\cercle.png')
         self.croixWin_image = PhotoImage(file='Morpion\img\croix_win.png')
@@ -18,19 +17,26 @@ class Morpion(Tk):
         self.bind("<Button>", self.analyser_position_clic)
         self.plateau = [0] * 10
         self.joueur = 1
+        self.Joueur1Win = 0
+        self.Joueur2Win = 0
+        
         self.victoire = False
- 
+        self.affichage()
+
     def changement_de_joueurs(self):
         if self.joueur == 1: 
             self.joueur = 2
         else:
             self.joueur = 1
  
-    def tracer_plateau(self):
+    def affichage(self):
         self.can.create_line(140, 20, 140, 400, width = 4)
         self.can.create_line(280, 20, 280, 400, width = 4)
         self.can.create_line(20, 140, 400, 140, width = 4)
         self.can.create_line(20, 280, 400, 280, width = 4)
+
+        self.can.create_text(140,550,text="Victoire Joueur X: "+str(self.Joueur1Win),font="Arial 10 bold")
+        self.can.create_text(280,550,text="Victoire Joueur O: "+str(self.Joueur2Win),font="Arial 10 bold")
 
     def tracer_croix(self, x, y):
         self.can.create_image(x,y,image=self.croix_image)
@@ -94,61 +100,69 @@ class Morpion(Tk):
                 if self.joueur == 1:
                     self.plateau[i] = "✖"
                     self.tracer_croix(self.x, self.y)
-                    self.test_gagnant("✖",self.croixWin_image)
+                    self.test_gagnant("✖",self.croixWin_image,self.Joueur1Win)
                 else:
                     self.plateau[i] = "⭘"
                     self.tracer_rond(self.x, self.y)
-                    self.test_gagnant("⭘",self.cercleWin_image)
+                    self.test_gagnant("⭘",self.cercleWin_image,self.Joueur2Win)
                 self.changement_de_joueurs()
 
-    def test_gagnant(self,symbole,img):
+    def test_gagnant(self,symbole,img,joueur):
         if self.plateau[1] == self.plateau[2] == self.plateau[3] == symbole and self.plateau[1]!= 0:
             self.can.create_image(70,70,image=img)
             self.can.create_image(210,70,image=img)
             self.can.create_image(350,70,image=img)
             self.victoire = True
+            joueur += 1
  
         elif self.plateau[4] == self.plateau[5] == self.plateau[6] == symbole and self.plateau[4]!= 0:
             self.can.create_image(70,210,image=img)
             self.can.create_image(210,210,image=img)
             self.can.create_image(350,210,image=img)
             self.victoire = True
+            joueur += 1
 
         elif self.plateau[7] == self.plateau[8] == self.plateau[9] == symbole and self.plateau[7]!= 0:
             self.can.create_image(70,350,image=img)
             self.can.create_image(210,350,image=img)
             self.can.create_image(350,350,image=img)
             self.victoire = True
+            joueur += 1
 
         elif self.plateau[1] == self.plateau[5] == self.plateau[9] == symbole and self.plateau[1]!= 0:
             self.can.create_image(70,70,image=img)
             self.can.create_image(210,210,image=img)
             self.can.create_image(350,350,image=img)
             self.victoire = True
+            joueur += 1
 
         elif self.plateau[3] == self.plateau[5] == self.plateau[7] == symbole and self.plateau[3]!= 0:
             self.can.create_image(70,350,image=img)
             self.can.create_image(210,210,image=img)
             self.can.create_image(350,70,image=img)
             self.victoire = True
+            joueur += 1
 
         elif self.plateau[1] == self.plateau[4] == self.plateau[7] == symbole and self.plateau[1]!= 0:
             self.can.create_image(70,70,image=img)
             self.can.create_image(70,210,image=img)
             self.can.create_image(70,350,image=img)
             self.victoire = True
+            joueur += 1
 
         elif self.plateau[2] == self.plateau[5] == self.plateau[8] == symbole and self.plateau[2]!= 0:
             self.can.create_image(210,70,image=img)
             self.can.create_image(210,210,image=img)
             self.can.create_image(210,350,image=img)
             self.victoire = True
+            joueur += 1
 
         elif self.plateau[3] == self.plateau[6] == self.plateau[9] == symbole and self.plateau[3]!= 0:
             self.can.create_image(350,70,image=img)
             self.can.create_image(350,210,image=img)
             self.can.create_image(350,350,image=img)
             self.victoire = True
+            joueur += 1
 
  
 
