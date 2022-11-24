cc0c07c8d9146bf210343d1a50636f73c4461f78
Enzo P <ephilippe@gaming.tech>
2022-11-24 17:20:13 +0100

    t

--- Morpion/pierreFeuilleCiseau.py
+++ Morpion/pierreFeuilleCiseau.py
@@ -4,138 +4,63 @@ from random import *
 #Definir une fonction pierreFeuilleCiseau
 def pierreFeuilleCiseau():
     #Afficher les choix possible 1(Pierre), 2(Feuille), 3(Ciseau)
-    print("Choisissez un chiffre: 1 pour Pierre, 2 pour Feuille, 3 pour Ciseau")
+    print("Chose a number, 1 for Rock, 2 for Scissor, 3 for Paper")
     #Assigner a choixJoueur le choix du joueur grace à la fonction input
     choixJoueur = int(input(""))
     #Assigner a choixOrdi le retour de la fonction Random
     choixOrdi = randint(1,3)
 
-    #Initialiser une variable nombreVictoire à 0
-    nombreVictoire = 0
-    #Initialiser une variable nombreDefaite à 0
-    nombreDefaite = 0
-    #Initialiser une variable nombreEgalite à 0
-    nombreEgalite = 0
-    #Initialiser une variable nombrePartie à 0
-    nombrePartie = 0
-    #Initialiser une variable continuerPartie à True
-    continuerPartie = True
-
-    #Tant que continuerPartie est True
-    while continuerPartie:
-       
-        #Alors si choixJoueur est égal à choixOrdi
-        if choixJoueur == choixOrdi:
-            #Alors afficher "Egalité"
-            print("\nIl y a Egalité")
-            #Incrementer la variable nombreEgalite de 1
-            nombreEgalite += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
+    if choixJoueur == choixOrdi:
+        #Alors afficher "Egalité"
+        print("\nIt's a tie... Again\n")
+        print("¯\_(ツ)_/\n")
+        print("Mais ce n'est pas grave car vous savez, moi je ne crois pas qu’il y ait de bonne ou de mauvaise situation. Moi, si je devais résumer ma vie aujourd’hui avec vous, je dirais que c’est d’abord des rencontres, des gens qui m’ont tendu la main, peut-être à un moment où je ne pouvais pas, où j’étais seul chez moi. Et c’est assez curieux de se dire que les hasards, les rencontres forgent une destinée… Parce que quand on a le goût de la chose, quand on a le goût de la chose bien faite, le beau geste, parfois on ne trouve pas l’interlocuteur en face, je dirais, le miroir qui vous aide à avancer. Alors ce n’est pas mon cas, comme je le disais là, puisque moi au contraire, j’ai pu ; et je dis merci à la vie, je lui dis merci, je chante la vie, je danse la vie… Je ne suis qu’amour ! Et finalement, quand beaucoup de gens aujourd’hui me disent : « Mais comment fais-tu pour avoir cette humanité ? » Eh bien je leur réponds très simplement, je leur dis que c’est ce goût de l’amour, ce goût donc qui m’a poussé aujourd’hui à entreprendre une construction mécanique, mais demain, qui sait, peut-être simplement à me mettre au service de la communauté, à faire le don, le don de soi…")
         
-        #Si choixJoueur est 1 et choixOrdi est 2
-        elif choixJoueur == 1 and choixOrdi == 2:
-            #Alors afficher choixJoueur et choixOrdi
-            print("\nPierre Contre Feuille")
-            #Afficher "Défaite du Joueur"
-            print("Défaite du joueur")
-            #Incrementer la variable nombreDefaite de 1
-            nombreDefaite += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
+    #Si choixJoueur est 1 et choixOrdi est 2
+    elif choixJoueur == 1 and choixOrdi == 2:
+        #Alors afficher choixJoueur et choixOrdi
+        print("\nPierre Contre Feuille")
+        #Afficher "Défaite du Joueur"
+        print("Défaite du joueur")
 
-        #Sinon si choixJoueur est 1 et choixOrdi est 3
-        elif choixJoueur == 1 and choixOrdi == 3:
-            #Alors afficher choixJoueur et choixOrdi
-            print("\nPierre Contre Ciseau")
-            #Afficher "Victoire du Joueur"
-            print("Victoire du Joueur")
-            #Incrementer la variable nombreVictoire de 1
-            nombreVictoire += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
+    #Sinon si choixJoueur est 1 et choixOrdi est 3
+    elif choixJoueur == 1 and choixOrdi == 3:
+        #Alors afficher choixJoueur et choixOrdi
+        print("\nPierre Contre Ciseau")
+        #Afficher "Victoire du Joueur"
+        print("Victoire du Joueur")
 
-        #Sinon si choixJoueur est 2 et choixOrdi est 1
-        elif choixJoueur == 2 and choixOrdi == 1:
-            #Alors afficher choixJoueur et choixOrdi
-            print("\nFeuille Contre Pierre")
-            #Afficher "Victoire du Joueur"
-            print("Victoire Joueur")
-            #Incrementer la variable nombreVictoire de 1
-            nombreVictoire += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
+    #Sinon si choixJoueur est 2 et choixOrdi est 1
+    elif choixJoueur == 2 and choixOrdi == 1:
+        #Alors afficher choixJoueur et choixOrdi
+        print("\nFeuille Contre Pierre")
+        #Afficher "Victoire du Joueur"
+        print("Victoire Joueur")
 
-        #Sinon si choixJoueur est 2 et choixOrdi est 3
-        elif choixJoueur == 2 and choixOrdi ==3:
-            #Alors afficher choixJoueur et choixOrdi
-            print("\nFeuille Contre Ciseau")
-            #Afficher "Défaite du Joueur"
-            print("Défaite du joueur")
-            #Incrementer la variable nombreDefaite de 1
-            nombreDefaite += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
+    #Sinon si choixJoueur est 2 et choixOrdi est 3
+    elif choixJoueur == 2 and choixOrdi ==3:
+        #Alors afficher choixJoueur et choixOrdi
+        print("\nFeuille Contre Ciseau")
+        #Afficher "Défaite du Joueur"
+        print("Défaite du joueur")
         
-        #Sinon si choixJoueur est 3 et choixOrdi est 1
-        elif choixJoueur == 3 and choixOrdi == 2:
-            #Alors afficher choixJoueur et choixOrdi
-            print("\nCiseau Contre Feuille")
-            #Afficher "Victoire du Joueur"
-            print("Victoire du joueur")
-            #Incrementer la variable nombreVictoire de 1
-            nombreVictoire += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
+    #Sinon si choixJoueur est 3 et choixOrdi est 1
+    elif choixJoueur == 3 and choixOrdi == 2:
+        #Alors afficher choixJoueur et choixOrdi
+        print("\nCiseau Contre Feuille")
+        #Afficher "Victoire du Joueur"
+        print("Victoire du joueur")
 
-        #Sinon si choixJoueur est 3 et choixOrdi est 2
-        elif choixJoueur == 3 and choixOrdi == 1:
-            #Alors afficher choixJoueur et choixOrdi
-            print("\nCiseau Contre Pierre")
-            #Afficher "Défaite du Joueur"
-            print("Défaite du joueur")
-            #Incrementer la variable nombreDefaite de 1
-            nombreDefaite += 1
-            #Incrementer la variable nombrePartie de 1
-            nombrePartie += 1
 
-        else:
-            print("Erreur, vous n'avez pas rentrez un choix valide")
+    #Sinon si choixJoueur est 3 et choixOrdi est 2
+    elif choixJoueur == 3 and choixOrdi == 1:
+        #Alors afficher choixJoueur et choixOrdi
+        print("\nCiseau Contre Pierre")
+        #Afficher "Défaite du Joueur"
+        print("Défaite du joueur")
 
-        
-        #Assigner a continuer le retour de l'execution de la fonction input
-        print("\nVoulez-vous refaire une partie ?")
-        continuer = input("")
-        #Si la reponse est non
-        if continuer.upper() == "NON":
-            #Alors changer la variable continuerPartie à False
-            continuerPartie =False
-        #Sinon 
-        else:
-            #Afficher "Votre nombre de partie est:" nombrePartie
-            print("\nVotre nombre de partie est:", nombrePartie)
-            #Afficher "Votre nombre de victoire est:" nombreVictoire
-            print("Votre nombre de victoire est:", nombreVictoire)
-            #Afficher "Votre nombre de Defaite est:" nombreDefaite
-            print("Votre nombre de Defaite est:", nombreDefaite)
-            #Afficher "Votre nombre d'Egalité est:" nombreEgalite
-            print("Votre nombre d'Egalité est:", nombreEgalite)
-            #Afficher les choix possible 1(Pierre), 2(Feuille), 3(Ciseau)
-            print("\nChoisissez un chiffre: 1 pour Pierre, 2 pour Feuille, 3 pour Ciseau")
-            #Assigner a choixJoueur le choix du joueur grace à la fonction input
-            choixJoueur = int(input(""))
-            #Assigner a choixOrdi le retour de la fonction Random
-            choixOrdi = randint(1,3)
-            print(choixOrdi)
+    else:
+        print("Erreur, vous n'avez pas rentrez un choix valide")
 
-    
-    #Afficher "Votre nombre de partie est:" nombrePartie
-    print("Votre nombre de partie est:", nombrePartie)
-    #Afficher "Votre nombre de victoire est:" nombreVictoire
-    print("Votre nombre de victoire est:", nombreVictoire)
-    #Afficher "Votre nombre de Defaite est:" nombreDefaite
-    print("Votre nombre de Defaite est:", nombreDefaite)
-    #Afficher "Votre nombre d'Egalité est:" nombreEgalite
-    print("Votre nombre d'Egalité est:", nombreEgalite)
 
 #FIN
